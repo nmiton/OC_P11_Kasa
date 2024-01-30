81364351f1dec06599e2ed58f2a26762e4cdae71
Nathan <nathan.miton@orange.fr>
2024-01-30 16:37:21 +0100

    maj

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,18 +16,12 @@ export default function Header() {
 			<img src={Logo} alt="Logo Kasa" className="header__img" onClick={handleClickIcon} />
 
 			<nav className="header__navigation">
-				<ul>
-					<li>
-						<NavLink to="/" className={({ isActive, isPending }) => (isPending ? "pending" : isActive ? "active" : "")}>
-							Accueil
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to="/a-propos" className={({ isActive, isPending }) => (isPending ? "pending" : isActive ? "active" : "")}>
-							A Propos
-						</NavLink>
-					</li>
-				</ul>
+				<NavLink to="/" className={({ isActive, isPending }) => (isPending ? "pending" : isActive ? "active" : null)}>
+					Accueil
+				</NavLink>
+				<NavLink to="/a-propos" className={({ isActive, isPending }) => (isPending ? "pending" : isActive ? "active" : null)}>
+					A Propos
+				</NavLink>
 			</nav>
 		</header>
 	);
