11b576da4f8a9fe4a22af38a65a6a025a0ba537e
nmiton <nathan.miton@orange.fr>
2024-10-04 13:58:52 +0200

    maj

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,7 +1,7 @@
 import Banner from "../components/Banner";
 import Dropdown from "../components/Dropdown";
 
-export default function AboutUs(params) {
+export default function AboutUs() {
 	const detailsFiabilite =
 		"Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.";
 	const detailsRespect =
@@ -11,6 +11,7 @@ export default function AboutUs(params) {
 
 	const detailsSecurite =
 		"La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.";
+
 	return (
 		<div className="about__page">
 			<Banner src="assets/img/banner_img_2.png" alt="Chez vous, partout et ailleurs" />
